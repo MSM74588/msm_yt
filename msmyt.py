c49965b3aff54fe9af9bc8738aae4902f8d5e201
Spike_Projects <msm74588@gmail.com>
2021-08-09 03:21:32 +0530

    Win inhibitor test

--- msmyt.py
+++ msmyt.py
@@ -11,14 +11,54 @@ from rich.console import Console
 from rich.markdown import Markdown
 # from tqdm import tqdm  
 # import time
+# CODE to INHIBIT SLEEP
 import os
+import ctypes
 
-osSleep = None
-    # in Windows, prevent the OS from sleeping while we run
-    # do slow stuff
+class WindowsInhibitor:
+    '''
+    Prevent OS sleep/hibernate in windows; code from:
+    https://github.com/h3llrais3r/Deluge-PreventSuspendPlus/blob/master/preventsuspendplus/core.py
+    API documentation:
+    https://msdn.microsoft.com/en-us/library/windows/desktop/aa373208(v=vs.85).aspx
+    '''
+    ES_CONTINUOUS        = 0x80000000
+    ES_SYSTEM_REQUIRED   = 0x00000001
+    ES_AWAYMODE_REQUIRED = 0x00000040
 
-if osSleep:
-        osSleep.uninhibit()
+    def __init__(self):
+        pass
+
+    def inhibit(self):
+        if os.name == 'nt': #Prevent Windows from going to sleep
+            try:
+                ctypes.windll.kernel32.SetThreadExecutionState(
+                    WindowsInhibitor.ES_CONTINUOUS | \
+                    WindowsInhibitor.ES_SYSTEM_REQUIRED)
+            except:
+                return False
+            return True
+        else:
+            return False
+
+    def uninhibit(self):
+        import ctypes
+        #print("")
+        if os.name == 'nt': #Allow Windows to go to sleep
+            try:
+                ctypes.windll.kernel32.SetThreadExecutionState(
+                    WindowsInhibitor.ES_CONTINUOUS)
+            except:
+                return False
+            return True
+        else:
+            return False
+
+
+
+    ####
+
+# //CODE to INHIBIT SLEEP
 
 console = Console()
 
